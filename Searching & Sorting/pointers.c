59b417434dcd6b7f0a0cc244ecf96c656c579e1f
rushabh782 <155352920+rushabh782@users.noreply.github.com>
2024-03-18 21:23:19 +0530

    Add files via upload

---
+++ Searching & Sorting/pointers.c
@@ -0,0 +1,17 @@
+/*WAP for pointers*/
+#include<stdio.h>
+int main()
+{
+	int *pt;//declaration of pointer to int
+	int a = 100;
+	float *fp;//declaration of pointer to float
+	float b = 90.99;
+	char *cp;//declaration of pointer to char
+	char bool = 'y';
+	pt=&a;//assigning address of a into pointer var pt
+	fp=&b;
+	cp=&bool;
+	printf("\nAddress of a = %u",pt);
+	printf("\nAddress of b = %u",fp);
+	printf("\nAddress of bool = %u",cp);
+}
